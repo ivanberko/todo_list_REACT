615ad4efa2109dc6935772ca5d4f2b7c2fa0527e
Ivan Berk <berkoivan@gmail.com>
2020-05-23 18:13:48 +0300

    Updates

---
+++ precache-manifest.b9fe887743fd3c6b09609eee0076e28a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d105219f75082a8f77f03aabb1f4dd1f",
+    "url": "/todo_list_REACT/index.html"
+  },
+  {
+    "revision": "875267d484a3142b3caf",
+    "url": "/todo_list_REACT/static/css/main.23d4a3f4.chunk.css"
+  },
+  {
+    "revision": "0c9bff183c5eb2195161",
+    "url": "/todo_list_REACT/static/js/2.0ee2df79.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo_list_REACT/static/js/2.0ee2df79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "875267d484a3142b3caf",
+    "url": "/todo_list_REACT/static/js/main.2edfd410.chunk.js"
+  },
+  {
+    "revision": "02eb87b2c0886c431093",
+    "url": "/todo_list_REACT/static/js/runtime-main.8d8a98c9.js"
+  }
+]);
